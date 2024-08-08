9e829a33109da7ec8bdff3c2d20de669ced3a1c0
Kavitha134 <ankavitha134@gmail.com>
2024-08-08 15:50:24 +0530

    Create variable.tf

---
+++ Terraform/module_vm/variable.tf
@@ -0,0 +1,10 @@
+variable "machine_type" {
+    description = "This stores te machine type"
+    type = string
+}
+
+variable "image_name" {
+    description = "This stores the image name"
+    type = string
+  
+}
